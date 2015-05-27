763949b8c0e67e632d0c03ebbc90dfbe825ea844
Dan Tello <dan.tello@gmail.com>
2015-05-27 10:41:55 -0400

    [update] Build task + specify engines in package.json

--- gulpfile.js/tasks/build-production.js
+++ gulpfile.js/tasks/build-production.js
@@ -3,5 +3,5 @@ var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:production', function(cb) {
   process.env.NODE_ENV = 'production'
-  gulpSequence('karma', 'clean', ['fonts', 'iconFont', 'images', 'audio'], ['sass', 'webpack:production'], 'html', 'rev', cb);
+  gulpSequence('clean', ['fonts', 'iconFont', 'images', 'audio'], ['sass', 'webpack:production'], 'html', 'rev', cb);
 });
